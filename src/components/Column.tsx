3f5a3cd3a83e3f5a6e2aa6aa8774ab3bcb78d0b3
devgabriellopes <gabriel.lopes.nascimento.dev@gmail.com>
2025-02-14 14:49:12 -0300

    fix: tamanho do board

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -9,9 +9,9 @@ interface ColumnProps {
 
 export default function Column({ title, tasks }: ColumnProps) {
   return (
-    <div className="bg-gray-200 p-4 rounded-lg shadow-md w-full md:w-64 flex-shrink-0">
+    <div className="bg-gray-200 p-4 rounded-lg shadow-md w-full h-80 md:w-64 flex-shrink-0">
       <h2 className="font-bold mb-4">{title}</h2>
-      <Droppable droppableId={title} key={title}>
+      <Droppable droppableId={title} key={title} type="group">
         {(provided) => (
           <div {...provided.droppableProps} ref={provided.innerRef} className="space-y-2 min-h-[100px]">
             {tasks.map((task, index) => (
